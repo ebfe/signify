d4446c9953a6e51cd4012a43ebdaa6d59e7f6bd1
Michael Gehring <mg@ebfe.org>
2014-02-13 02:35:24 +0100

    add parsed binary test data

--- signify.go
+++ signify.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"github.com/agl/ed25519"
 	"io"
 	"strings"
 )
@@ -12,6 +13,28 @@ import (
 const (
 	commentHdr = "untrusted comment: "
 )
+
+type encryptedKey struct {
+	PKAlgo      [2]byte
+	KDFAlgo     [2]byte
+	KDFRounds   uint32
+	Salt        [16]byte
+	Checksum    [8]byte
+	Fingerprint [8]byte
+	PrivateKey  [ed25519.PrivateKeySize]byte
+}
+
+type pubkey struct {
+	PKAlgo      [2]byte
+	Fingerprint [8]byte
+	PublicKey   [ed25519.PublicKeySize]byte
+}
+
+type sig struct {
+	PKAlgo      [2]byte
+	Fingerprint [8]byte
+	Signature   [ed25519.SignatureSize]byte
+}
 
 func ReadFile(r io.Reader) (comment string, content []byte, err error) {
 	sc := bufio.NewScanner(r)
